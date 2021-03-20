1b72f3041451ff6cb229bf66b63030a59818631a
curt <curt.k@kakaomobility.com>
2021-03-21 00:29:03 +0900

    network_programming추가

---
+++ network_programming/02_file/open_fd.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>     // EXIT_FAILURE, EXIT_SUCCESS
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+
+int main()
+{
+  int fd;
+  char buf_msg[64];
+  printf("low level file handle\n");
+
+  if ((fd = open("fd_test.log", O_CREAT | O_WRONLY, 0644)) == -1) {
+    fprintf(stderr, "Fail: open: %d(%s)\n", errno, strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+
+  dprintf(fd, "PID[%d] Low level file handle\n", getpid());
+  strcpy(buf_msg, "write: Test message\n");
+  write(fd, buf_msg, strlen(buf_msg));
+  close(fd);
+  return EXIT_SUCCESS;
+
+}
